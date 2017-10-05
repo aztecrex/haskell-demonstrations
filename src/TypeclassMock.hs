f9f048819f04d24c2f32bd7e96c58ae50ea54287
Greg Wiley <aztec.rex@jammm.com>
2017-10-04 21:40:48 -0700

    state mock

--- src/TypeclassMock.hs
+++ src/TypeclassMock.hs
@@ -5,18 +5,30 @@ module TypeclassMock (demos) where
 import Test.Tasty (testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 
-import Control.Monad.Trans.Writer (Writer, tell)
+import Control.Monad.Trans.Writer (execWriter, Writer, tell)
+import Control.Monad.Trans.State
+
 import Missiles (Missiles, launch)
 
 type WriterMock e = Writer [e]
 type WriteStringMock = WriterMock String
 
+type StateMock s = State s
+type StateBoolMock = StateMock Bool
+
+
 demos = testGroup "Typeclass Mock" [
         testCase "Simple Writer" $
-            (backdoor :: WriteStringMock ()) @?= tell ["launched"]
+            execWriter backdoor @?= ["launched"]
+       , testCase "Simple State" $
+            execState backdoor False  @?= True
     ]
 
 instance Missiles WriteStringMock where
     launch = tell ["launched"]
 
+instance Missiles StateBoolMock where
+    launch = put True
+
+backdoor :: Missiles m => m ()
 backdoor = launch
