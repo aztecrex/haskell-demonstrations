ded0a54ad8c9c0fc3b2e6f499444709876780e66
Greg Wiley <gwiley@cj.com>
2017-10-31 12:08:12 -0700

    improve di example

--- src/DependencyInjection.hs
+++ src/DependencyInjection.hs
@@ -9,36 +9,40 @@ import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Writer (WriterT, tell, execWriterT)
 import Missiles (Missiles(..))
 
-
-
 demos :: TestTree
 demos = testGroup "DependencyInjection" [
         testCase "here be stubs" $
-            let program = barage :: Stub ()                -- given injected
-                actual = (execWriterT program) 17          -- when run with injected
-            in actual @?= take 17 (repeat "launchc0de")    -- then ...
+            let size = 17
+                program = barage :: Stub ()                  -- inject with stub and mock
+                actual = execWriterT program size            -- when injected program is run
+            in actual @?= take size (repeat "launchc0de")    -- then ...
     ]
 
-class (Monad m) => Rand m where
-    fromRange :: Int -> Int -> m Int
-
+-- code with side-effectful dependencies
+-- barage picks a random number and launches that many
+-- missiles
 barage :: (Missiles m, Rand m) => m ()
 barage = do
     size <- fromRange 12 25
     replicateM_ size (launch "launchc0de")
 
+-- an interface to a module that probably has side effects
+class (Monad m) => Rand m where
+    fromRange :: Int -> Int -> m Int
 
 -- a stub implementation of Rand
+-- simply returns whatever number it is given
 instance Rand ((->) Int) where
     fromRange lo hi = (clamp lo hi) . id where
         clamp lo' hi' x = min hi' (max lo' x)
 
--- a stub implementation of Missiles
+-- a mock implementation of Missiles
+-- keep track of all the launches
 instance (Monad m) => Missiles (WriterT [String] m) where
-    launch p = tell [p] >> pure ()
+    launch p = tell [p]
 
--- custom stub for both
+-- custom mock + stub for missiles and random number
 type Stub = WriterT [String] ((->) Int)
+
 instance Rand Stub where
     fromRange l h = lift $ fromRange l h
-
